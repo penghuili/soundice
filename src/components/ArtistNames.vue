2b9eb5c419b6d5383b377a35787d0ee1e6d1125d
Peng <ichpenghui@gmail.com>
2026-08-13 21:26:58 +0200

    Add Spotify artist links to album cards

--- src/components/ArtistNames.vue
+++ src/components/ArtistNames.vue
@@ -12,7 +12,20 @@ defineProps({
   <template v-if="artists.length">
     <template v-for="(artist, index) in artists" :key="artist.id || artist.name">
       <span v-if="index">, </span>
-      <AiLookupLink :href="artistAiModeUrl(artist.name)" :label="artist.name" />
+      <span class="artist-name-with-link">
+        <AiLookupLink :href="artistAiModeUrl(artist.name)" :label="artist.name" />
+        <a
+          v-if="artist.url"
+          class="artist-spotify-link"
+          :href="artist.url"
+          target="_blank"
+          rel="noreferrer"
+          :aria-label="`Open ${artist.name} on Spotify`"
+          :title="`Open ${artist.name} on Spotify`"
+        >
+          Spotify ↗
+        </a>
+      </span>
     </template>
   </template>
   <template v-else>{{ fallback }}</template>
